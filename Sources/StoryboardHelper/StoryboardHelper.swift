0aec8bb79084a0cdcaa7d53e3f148d0675b1ffc7
manaes <pakjuwan@naver.com>
2024-12-08 16:51:20 +0900

    func controller -> MainActor 전환 / static으로 변경

--- Sources/StoryboardHelper/StoryboardHelper.swift
+++ Sources/StoryboardHelper/StoryboardHelper.swift
@@ -12,78 +12,79 @@ enum StoryboardError: Error {
 }
 
 @MainActor
-final class Storyboard: NSObject {
-  /// Shared singleton instance with board names
-  private static let shared = Storyboard()
-  /// Exclude storyboard
-  private var excludeBoards = ["LaunchScreen"]
-  /// Loaded storyboad name list
-  private var boards: Set<String> = []
-  /// Bundle path
-  private var bundle: Bundle =
-  if ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil {
-    Bundle.module
-  } else {
-    Bundle.main
-  }
-  
-  /**
-   *  Load storyboard fIle names
-   */
-  private func checkBoardList() {
-    if Storyboard.shared.boards.isEmpty {
-      let filesEnumerator = FileManager.default.enumerator(atPath: bundle.bundlePath)!
-      while let file = filesEnumerator.nextObject() as? String {
-        if let name = getStorybaordName(file), !excludeBoards.contains(name) {
-          Storyboard.shared.boards.insert(name)
-        }
-      }
+final public class Storyboard: NSObject {
+    /// Shared singleton instance with board names
+    private static let shared = Storyboard()
+    /// Exclude storyboard
+    private let excludeBoards = ["LaunchScreen"]
+    /// Loaded storyboad name list
+    private var boards: Set<String> = []
+    /// Bundle path
+    private static var bundle: Bundle =
+    if ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil {
+        Bundle.module
+    } else {
+        Bundle.main
     }
-  }
-  
-  /**
-   *  Geet storyboard name
-   */
-  func getStorybaordName(_ fileName: String) -> String? {
-    if !fileName.hasSuffix(".storyboardc") { return nil }
-    return fileName.components(separatedBy: ".storyboardc").first
-  }
-  
-  /**
-   Get viewController from storyboard with storyboard ID
-   (must same storyboard ID and class name)
-   
-   Example:
-   
-   ```
-   let vc = try Storyboard().controller(TestViewController.self)
-   ```
-   
-   ```
-   let vc = try Storyboard().controller(TestViewController.self) { coder in
-   TestViewController(
-       viewModel: TestViewModel(coordinator: self),
-       coder: coder
-    )
-   }
-   ```
-   
-   - parameter from: ViewController's storyboard  ID (== class name)
-   
-   - returns: ViewController object
-   */
-  public func controller<T: UIViewController>(_ from: T.Type, creator: ((NSCoder) -> T?)? = nil) throws -> T {
-    checkBoardList()
-    let name = String(describing: from)
-    for sotyrboardName in Storyboard.shared.boards {
-      let storyboard = UIStoryboard(name: sotyrboardName, bundle: bundle)
-      if let availableIdentifiers = storyboard.value(forKey: "identifierToNibNameMap") as? [String: String], availableIdentifiers[name] != nil {
-        if let creator {
-          return storyboard.instantiateViewController(identifier: name, creator: creator)
+    
+    /**
+     *  Load storyboard fIle names
+     */
+    private static func checkBoardList() {
+        if Storyboard.shared.boards.isEmpty {
+            let filesEnumerator = FileManager.default.enumerator(atPath: Storyboard.bundle.bundlePath)!
+            while let file = filesEnumerator.nextObject() as? String {
+                if let name = Storyboard.getStorybaordName(file), !Storyboard.shared.excludeBoards.contains(name) {
+                    Storyboard.shared.boards.insert(name)
+                }
+            }
         }
-        return storyboard.instantiateViewController(withIdentifier: name) as! T
-      }
     }
-    throw StoryboardError.notFound
-  }
+    
+    /**
+     *  Geet storyboard name
+     */
+    private static func getStorybaordName(_ fileName: String) -> String? {
+        if !fileName.hasSuffix(".storyboardc") { return nil }
+        return fileName.components(separatedBy: ".storyboardc").first
+    }
+    
+    /**
+     Get viewController from storyboard with storyboard ID
+     (must same storyboard ID and class name)
+     
+     Example:
+     
+     ```
+     let vc = try Storyboard().controller(TestViewController.self)
+     ```
+     
+     ```
+     let vc = try Storyboard().controller(TestViewController.self) { coder in
+     TestViewController(
+     viewModel: TestViewModel(coordinator: self),
+     coder: coder
+     )
+     }
+     ```
+     
+     - parameter from: ViewController's storyboard  ID (== class name)
+     
+     - returns: ViewController object
+     */
+    @MainActor
+    public static func controller<T: UIViewController>(_ from: T.Type, creator: ((NSCoder) -> T?)? = nil) throws -> T {
+        Storyboard.checkBoardList()
+        let name = String(describing: from)
+        for sotyrboardName in Storyboard.shared.boards {
+            let storyboard = UIStoryboard(name: sotyrboardName, bundle: bundle)
+            if let availableIdentifiers = storyboard.value(forKey: "identifierToNibNameMap") as? [String: String], availableIdentifiers[name] != nil {
+                if let creator {
+                    return storyboard.instantiateViewController(identifier: name, creator: creator)
+                }
+                return storyboard.instantiateViewController(withIdentifier: name) as! T
+            }
+        }
+        throw StoryboardError.notFound
+    }
 }
